510ae3dfc119d5a51eb639685e87fb141734968c
zzzhhh1 <58203962+zzzhhh1@users.noreply.github.com>
2026-06-11 09:49:16 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -25,7 +25,7 @@ SOURCE_URLS = [
     "https://raw.githubusercontent.com/roosterkid/openproxylist/main/V2RAY_BASE64.txt",
     "https://app.sublink.works/x/ZrVEXNV",
     "https://gcore.jsdelivr.net/gh/aews/jd2/v20528.txt",
-    "https://mm.mibei77.com/202606/06.0564bacrt.txt",
+    "https://freev2ray.top/V2rayN061456NO.txt",
     "https://mm.mibei77.com/202606/06.0664bacrt.txt",
     "https://mm.mibei77.com/202606/06.0764bacrt.txt",
     "https://mm.mibei77.com/202606/06.0864bacrt.txt",
